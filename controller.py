66dd39f1bcf15227f92df038d6ddb649302b6f77
Harrison Hubbell <harrison.hubbell@usvt-its-hhub.(none)>
2014-07-17 19:42:23 -0400

    [ADD] 201 HTTP response

--- controller.py
+++ controller.py
@@ -2,26 +2,39 @@
 # Harrison Hubbell
 # An HTTP controller for the BBB Workshop
 
-import SimpleHTTPServer
-import SocketServer
+import BaseHTTPServer
+import time
 
+CLIENT_IDS = {}
 
-class TCPHandler(SocketServer.BaseRequestHandler):
+class TCPHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     
-    def handle(self):
-        self.data = self.request.recv(1024).strip()
-        
+    def ack(self):
+        return 0
+
+    def add_client(self):
+        CLIENT_IDS[self.client_address[0]] = {}
+
+    def do_GET(self):
+        print "ACK"
+        self.data = self.headers.items()
+
+        if self.client_address[0] not in CLIENT_IDS.keys():
+            self.add_client()
+            self.ack()
+
         print "{} wrote:".format(self.client_address[0])
         print self.data
 
-        self.request.sendall(self.data.upper()) 
+        self.send_response(201)
+
+
 
 
 if __name__ == "__main__":
     HOST = ""
     PORT = 8888
 
-    #handler = SimpleHTTPServer.SimpleHTTPRequestHandler
-    httpd   = SocketServer.TCPServer((HOST, PORT), TCPHandler)
+    httpd   = BaseHTTPServer.HTTPServer((HOST, PORT), TCPHandler)
 
     httpd.serve_forever()
